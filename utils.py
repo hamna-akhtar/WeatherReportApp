c4d4fef2a3583a6c55de5fb17684416f07d017cc
hamna-akhtar <hamna.akhtar@ignicube.com>
2025-07-30 14:52:37 +0500

    resolved reviews

--- utils.py
+++ utils.py
@@ -3,35 +3,43 @@ import pandas as pd
 import calendar
 
 
-def txt_to_csv(input_dir_path, files):
-    for filepath in files:
-        filename = os.path.splitext(filepath)[0]
-        if filepath.endswith('.txt'):
-            os.rename(os.path.join(input_dir_path, filepath), os.path.join(input_dir_path, filename + '.csv'))
+class ReadCsv:
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def csv_to_df(self, filepath):
+        df = pd.read_csv(os.path.join(self.dir_path, filepath))
+
+        if 'PKST' in df.columns:
+            df.rename(columns={'PKST': 'PKT'}, inplace=True)
+
+        if df.iloc[-1]['PKT'].startswith("<!--"):
+            df = df.iloc[:-1]
+
+        df['PKT'] = pd.to_datetime(df['PKT'], format='%Y-%m-%d', errors='coerce')
+        df['Max TemperatureC'] = pd.to_numeric(df['Max TemperatureC'], errors='coerce')
+        df['Min TemperatureC'] = pd.to_numeric(df['Min TemperatureC'], errors='coerce')
+        df[' Mean Humidity'] = pd.to_numeric(df[' Mean Humidity'], errors='coerce')
+
+        return df
 
 
-def csv_to_df(input_dir_path, filepath):
-    df = pd.read_csv(os.path.join(input_dir_path, filepath))
+class Util:
+    @staticmethod
+    def txt_to_csv(input_dir_path, files):
+        for filepath in files:
+            filename = os.path.splitext(filepath)[0]
+            if filepath.endswith('.txt'):
+                os.rename(os.path.join(input_dir_path, filepath), os.path.join(input_dir_path, filename + '.csv'))
 
-    if 'PKST' in df.columns:
-        df.rename(columns={'PKST': 'PKT'}, inplace=True)
+    @staticmethod
+    def get_month_name(month_no):
+        return calendar.month_name[int(month_no)][:3]
 
-    if df.iloc[-1]['PKT'].startswith("<!--"):
-        df = df.iloc[:-1]
+    @staticmethod
+    def red(s):
+        return f"\033[91m{s}\033[00m"
 
-    df['PKT'] = pd.to_datetime(df['PKT'], format='%Y-%m-%d', errors='coerce')
-    df['Max TemperatureC'] = pd.to_numeric(df['Max TemperatureC'], errors='coerce')
-    df['Min TemperatureC'] = pd.to_numeric(df['Min TemperatureC'], errors='coerce')
-    df[' Mean Humidity'] = pd.to_numeric(df[' Mean Humidity'], errors='coerce')
-
-    return df
-
-
-def red(s):
-    return f"\033[91m{s}\033[00m"
-
-def blue(s):
-    return f"\033[96m{s}\033[00m"
-
-def get_month_name(month_no):
-    return calendar.month_name[int(month_no)][:3]
+    @staticmethod
+    def blue(s):
+        return f"\033[96m{s}\033[00m"
--- weatherman.py
+++ weatherman.py
@@ -1,73 +1,71 @@
 import argparse
+import sys
 from reportGenerator import *
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-e", help="Year for Report")
-parser.add_argument("-a", help="Year/Month for Report")
-parser.add_argument("-c", help="Year/Month for 2 bars")
-parser.add_argument("-f", help="Year/Month for 1 bar")
-parser.add_argument("d", help="Directory Path")
-args = parser.parse_args()
-print(args, '\n') #-----------------------------
 
-input_dir_path = args.d
-files = os.listdir(input_dir_path)
-txt_to_csv(input_dir_path, files)
+class HandleInputArgs:
+    def __init__(self, input_dir_path, files):
+        self.args = None
+        self.dir_path = input_dir_path
+        self.files = files
+        self.genReport = ReportGenerator(dir_path=self.dir_path, files=self.files)
 
-if args.a:
-    try:
-        year, month = args.a.split('/') if '/' in args.a else (args.a, None)
-        if month:
-            if len(year)>0:
-                print(f"Report for {year}/{month}")
-                get_year_month_report(input_dir_path, files, year, month)
-            else:
-                print("No year specified")
-        else:
-            print(f"Report for {year}")
-            get_year_report(input_dir_path, files, year)
-    except Exception as e:
-        print(e)
-
-elif args.e:
-    try:
-            year, month = args.e.split('/') if '/' in args.e else (args.e, None)
+    def handle_args_for_report(self, args):
+        self.args = args
+        try:
+            year, month = self.args.split('/') if '/' in self.args else (self.args, None)
             if month:
-                if len(year)>0:
-                    print(f"Report for {year}/{month}")
-                    get_year_month_report(input_dir_path, files, year, month)
+                if len(year) > 0:
+                    print(f"Report for {year}/{month}\n")
+                    self.genReport.get_year_month_report(year, month)
                 else:
                     print("No year specified")
             else:
-                print(f"Report for {year}:")
-                get_year_report(input_dir_path, files, year)
-    except Exception as e:
-        print(e)
+                print(f"Report for {year}\n")
+                self.genReport.get_year_report(year)
+        except Exception as e:
+            print(e)
 
-elif args.c:
-    try:
-        year, month = args.c.split('/') if '/' in args.c else (args.c, None)
-        if year and month:
-            print(f"Two bars each day for {year}/{month}:")
-            draw_bars(input_dir_path, files, year, month, "two")
-        else:
-            if not year:
-                print("No year specified")
-            if not month:
-                print("No month specified")
-    except Exception as e:
-        print(e)
 
-elif args.f:
-    try:
-        year, month = args.f.split('/') if '/' in args.f else (args.f, None)
-        if year and month:
-            print(f"Two bars each day for {year}/{month}:")
-            draw_bars(input_dir_path, files, year, month, "one")
-        else:
-            if not year:
-                print("No year specified")
-            if not month:
-                print("No month specified")
-    except Exception as e:
-        print(e)
+    def handle_args_for_bars(self, args, option):
+        self.args = args
+        try:
+            year, month = self.args.split('/') if '/' in self.args else (self.args, None)
+            if year and month:
+                self.genReport.draw_bars(year, month, option)
+            else:
+                if not year:
+                    print("No year specified")
+                if not month:
+                    print("No month specified")
+        except Exception as e:
+            print(e)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", help="Year for Report")
+    parser.add_argument("-a", help="Year/Month for Report")
+    parser.add_argument("-c", help="Year/Month for 2 bars")
+    parser.add_argument("-f", help="Year/Month for 1 bar")
+    parser.add_argument("d", help="Directory Path")
+    args = parser.parse_args()
+
+    input_dir_path = args.d
+    files = os.listdir(input_dir_path)
+    Util.txt_to_csv(input_dir_path, files)
+
+    inputHandler = HandleInputArgs(input_dir_path, files)
+    if args.a:
+        inputHandler.handle_args_for_report(args.a)
+    elif args.e:
+        inputHandler.handle_args_for_report(args.e)
+    elif args.c:
+        inputHandler.handle_args_for_bars(args.c, "two")
+    elif args.f:
+        inputHandler.handle_args_for_bars(args.f, "one")
+    else:
+        print("Not enough arguments given")
+
+
+if __name__ == '__main__':
+    sys.exit(main())
